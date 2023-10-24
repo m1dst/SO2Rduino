0c8d4b705baf9b78679bf768f99a1aca4bb2be32
dm4ff <58637397+dm4ff@users.noreply.github.com>
2023-10-24 12:54:58 +0200

    Update pins.h
    Pins to match  Relais board

--- Firmware/SO2RDuino/pins.h
+++ Firmware/SO2RDuino/pins.h
@@ -1,7 +1,7 @@
 // SO2Rduino - An SO2R Box built on an Arduino clone
 //
 // Copyright 2010, Paul Young
-//
+// mods by DF5RF 2023
 // This file contains all of the pin definitions and
 // macros to access them.
 
@@ -35,98 +35,107 @@
 // 
 // RX 1/2		D13	PB5
 
-#define SET_RX2              PORTB |= _BV(PB5)
-#define CLEAR_RX2            PORTB &= ~_BV(PB5)
+#define SET_RX2              digitalWrite(10, HIGH);digitalWrite(11, HIGH)  
+#define CLEAR_RX2            digitalWrite(10, LOW);digitalWrite(11, LOW)
 // RX Stereo	D11	PB3
 
-#define SET_STEREO           PORTB |= _BV(PB3)
-#define CLEAR_STEREO         PORTB &= ~_BV(PB3)
+#define SET_STEREO           digitalWrite(10, LOW);digitalWrite(11, HIGH)  // stereo is not mix!, Left =R1, Right=R2
+#define CLEAR_STEREO         digitalWrite(10, LOW);digitalWrite(11, LOW)
 
 // TX 1/2		D9	PB1
 
-#define SET_TX2               PORTB |= _BV(PB1)
-#define CLEAR_TX2             PORTB &= ~_BV(PB1)
+#define SET_TX2               digitalWrite(12, HIGH)
+#define CLEAR_TX2             
  
 // TX1 LED		D8	PB0
 
-#define SET_TX1               PORTB |= _BV(PB0)
-#define CLEAR_TX1             PORTB &= ~_BV(PB0)
+#define SET_TX1               digitalWrite(12, LOW)
+#define CLEAR_TX1             
+
+
+#define SET_PTT12V            digitalWrite(13, HIGH)
+#define CLR_PTT12V            digitalWrite(13, LOW)
+#define SET_CW_OUT            digitalWrite(8, HIGH)
+#define CLR_CW_OUT            digitalWrite(8, LOW)
 
 // RX1 LED		A4	PC4
 
-#define SET_RX1_LED           PORTC |= _BV(PC4)
-#define CLEAR_RX1_LED         PORTC &= ~_BV(PC4)
+#define SET_RX1_LED           
+#define CLEAR_RX1_LED         
 
 // RX2 LED		A5	PC5
 
-#define SET_RX2_LED           PORTC |= _BV(PC5)
-#define CLEAR_RX2_LED         PORTC &= ~_BV(PC5)
+#define SET_RX2_LED           
+#define CLEAR_RX2_LED         
 
 // PTT switch	D12	PB4
 
-#define GET_PTT_SWITCH        (PINB & _BV(PB4))
+#define GET_PTT_SWITCH        
 
 // PTT DTR		D2	PD2
 
-#define GET_PTT_DTR           (PIND & _BV(PD2))
+#define GET_PTT_DTR           
 
 // CW Key		D3	PD3
 
-#define GET_CW_KEY            (PIND & _BV(PD3))
-
+#define GET_CW_PADL            digitalRead(5)
+#define GET_CW_PADR            digitalRead(6)
+#define GET_CW_KEY             (!GET_CW_PADL | !GET_CW_PADR)
 // PTT TX 1	D4	PD4
 
-#define SET_PTT1              PORTD |= _BV(PD4)
+#define SET_PTT1              
 
 // PTT TX 2	D5	PD5
 
-#define SET_PTT2              PORTD |= _BV(PD5)
+#define SET_PTT2              
 
-#define CLEAR_PTT             PORTD &= ~(_BV(PD5) | _BV(PD4))
+#define CLEAR_PTT             
 
 // CW TX1		D6	PD6
 
-#define SET_CW1               PORTD |= _BV(PD6)
+#define SET_CW1               
 
 // CW TX2		D7	PD7
 
-#define SET_CW2               PORTD |= _BV(PD7)
+#define SET_CW2               
 
-#define CLEAR_CW              PORTD &= ~(_BV(PD6) | _BV(PD7))
+#define CLEAR_CW              
 
 // AUX CLK		D13	PB5
  
-#define SET_AUX_CLK           PORTB |= _BV(PB5)
+#define SET_AUX_CLK           
 
-#define CLEAR_AUX_CLK         PORTB &= ~(_BV(PB5))
+#define CLEAR_AUX_CLK         
 
 // AUX DATA	D11	PB3
 
-#define SET_AUX_DATA          PORTB |= _BV(PB3)
+#define SET_AUX_DATA          
 
-#define CLEAR_AUX_DATA        PORTB &= ~(_BV(PB3))
+#define CLEAR_AUX_DATA        
 
 // AUX STROBE	D10	PB2
 
-#define SET_AUX_STROBE        PORTB |= _BV(PB2)
+#define SET_AUX_STROBE        
 
-#define CLEAR_AUX_STROBE      PORTB &= ~(_BV(PB2))
+#define CLEAR_AUX_STROBE      
 
 // TX 1 Switch	A0	PC0
 
-#define GET_TX1_SWITCH        (PINC & _BV(PC0))
+#define GET_TX1_SWITCH        
+
+#define GET_TX_TOGGLE         digitalRead(4)
 
 // TX 2 Switch	A1	PC1
 
-#define GET_TX2_SWITCH        (PINC & _BV(PC1))
+#define GET_TX2_SWITCH        
  
 // RX 1 Switch	A2	PC2
 
-#define GET_RX1_SWITCH        (PINC & _BV(PC2))
+#define GET_RX1_SWITCH        digitalRead(2)
  
 // RX 2 Switch	A3	PC3
 
-#define GET_RX2_SWITCH        (PINC & _BV(PC3))
+#define GET_RX2_SWITCH        digitalRead(3)
  
 // The per-port direction and pull-ups
 
@@ -140,4 +149,3 @@
 #define PORT_D_PULL _BV(PD4) | _BV(PD5) | _BV(PD6) | _BV(PD7)
 
 #endif // PINS_H
-
